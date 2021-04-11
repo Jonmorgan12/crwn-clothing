06ab93a3b4b6280ef1700c60ba032e1843399482
Jon-Morgan <jonmorgan.cobb@gmail.com>
2021-04-10 18:07:25 -0700

    added addItem / removeItem functionality to checkout item component

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,7 +1,9 @@
-//action represents what we are trying to do to modify that new property which is our cartItems array
+//type of action represents what we are trying to do to modify that new property which is our cartItems array
 const CartActionTypes = {
   TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
   ADD_ITEM: "ADD_ITEM",
+  REMOVE_ITEM: "REMOVE_ITEM",
+  CLEAR_ITEM_FROM_CART: "CLEAR_ITEM_FROM_CART",
 };
 
 export default CartActionTypes;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,5 +1,5 @@
 export const addItemToCart = (cartItems, cartItemsToAdd) => {
-  //.fing() will go through the array and find the first item based on the condition that we pass in
+  //.find() will go through the array and find the first item based on the condition that we pass in
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === cartItemsToAdd.id
   );
@@ -14,4 +14,22 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
 
   //the cartItem is not found within the array, return new array with existing cartItems with a base quantity of 1
   return [...cartItems, { ...cartItemsToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemsToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemsToRemove.id
+  );
+
+  //if existingCartItem .quantity is equal to 1 remove it
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemsToRemove.id);
+  }
+
+  //otherwise decrease the quantity by one and keep every other cartItem the same because they do not need to be modified
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemsToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
