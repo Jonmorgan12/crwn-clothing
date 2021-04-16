ee3a0cd5b6ba1da98a83af1cee0cec083d276b91
Jon-Morgan <jonmorgan.cobb@gmail.com>
2021-04-16 13:01:24 -0700

    moved directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 // redux Component that controls all of the apps state
 import { Provider } from "react-redux";
-
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+// A store is a state container which holds the application’s state. Redux can have only a single store in your application
+import { store, persistor } from "./redux/store";
 
 import "./index.css";
 import App from "./App";
@@ -12,7 +13,9 @@ import App from "./App";
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>
   </Provider>,
   document.getElementById("root")
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,26 @@
+// Reducers are a pure function in Redux. Pure functions are predictable. Reducers are the only way to change states in Redux. It is the only place where you can write logic and calculations. Reducer function will accept the previous state of app and action being dispatched, calculate the next state and returns the new object.
+
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+//I want to use local storage
+import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
